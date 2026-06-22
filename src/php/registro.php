54ef8c03f7c1b573cdaebe09acd9b8dc2936250d
Bruno <ms2689992@gmail.com>
2026-06-21 21:30:57 -0300

    feat: Adicionando tabela de registros

---
+++ src/php/registro.php
@@ -0,0 +1,57 @@
+<?php
+
+    echo "<link rel='stylesheet' type='text/css' href='src/css/registro.css'/>";
+    include 'conexao.php';
+
+    $resultado = $cmd->query("SELECT * FROM tb_Usuario");
+    $total_registros = $resultado->rowCount();
+
+    if ($total_registros > 0)
+    {
+        echo "<body>";
+        echo "<h1>Tabela de Registros</h1>";
+        echo "<hr/>";
+        echo "<table>";
+        echo "<tr>
+                <th colspan=6>
+                    Dados Cadastrados
+                </th>
+              </tr>";
+        echo "<tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Senha</th>
+                <th>Telefone</th>
+                <th>Nível</th>
+              </tr>";
+
+        while ($linha = $resultado->fetch(PDO::FETCH_ASSOC))
+        {
+            $vId       = $linha['Id'];
+            $vNome     = $linha['Nome'];
+            $vEmail    = $linha['Email'];
+            $vSenha    = $linha['Senha'];
+            $vTelefone = $linha['Telefone'];
+            $vNivel    = $linha['Nivel'];
+
+            echo "<tr>
+                    <td>$vId</td>
+                    <td>$vNome</td>
+                    <td>$vEmail</td>
+                    <td>$vSenha</td>
+                    <td>$vTelefone</td>
+                    <td>$vNivel</td>
+                  </tr>";
+        }
+
+        echo "</table>";
+        echo "<br/><br/><br/>";
+        echo "<a href='../..//index.html'>Home</a>";
+        echo "</body>";
+    }
+    else
+    {
+        echo "<script language=javascript> window.alert('Não existem registros para exibir!!!'); location.href='index.html';</script>";
+    }
+?>
